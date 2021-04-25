b7e2663b0df96f6b3c2b87d599efb583a1829bc4
Daniele Pezzatini <daniele@bestiario.org>
2021-04-25 11:28:11 +0200

    Add collection download CSV

--- src/views/CollectionView.jsx
+++ src/views/CollectionView.jsx
@@ -2,13 +2,20 @@ import React, { useEffect, useState } from "react";
 import './CollectionView.sass';
 import {useCollection} from '../utils/collection'
 import ResourceExtendedInfo from '../components/ResourceExtendedInfo'
+import { CSVLink } from "react-csv";
+import * as Papa from 'papaparse'
 
 const CollectionView = () => {
   const [collection] = useCollection()
 
+  const csvData = Papa.unparse(collection.map(item => item.fields))
+
   return (
     <div className="collection-view">
-      <h2>Items Collected</h2>
+      <div className="collection-header">
+        <h2>Items Collected</h2>
+        <CSVLink data={csvData} filename={"ISCHE-collection.csv"} className="export-link">Export list</CSVLink>
+      </div>
       <div className="collection-grid">
       {
         collection.map(resource =>
