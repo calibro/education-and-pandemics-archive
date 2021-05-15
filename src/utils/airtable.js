00a6fcd3dadd068f67bc434c9b6a9fa5d7af10dd
Daniele Pezzatini <daniele@bestiario.org>
2021-05-15 16:15:27 +0200

    Connect production DB

--- src/utils/airtable.js
+++ src/utils/airtable.js
@@ -1,7 +1,7 @@
 import Airtable from 'airtable'
 
-const baseID = 'appVmbOc3ARWos22X' // TEST
-//const baseID = 'appyRkLfkVtG84rMU' // PRODUCTION
+//const baseID = 'appVmbOc3ARWos22X' // TEST
+const baseID = 'appyRkLfkVtG84rMU' // PRODUCTION
 
 export const base = new Airtable({
   apiKey:process.env.REACT_APP_AIRTABLE_API_KEY
