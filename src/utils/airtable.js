860b428611a323844c53462c4cae355d41b02bed
Daniele Pezzatini <daniele@bestiario.org>
2021-05-15 11:58:20 +0200

    Connected live data

--- src/utils/airtable.js
+++ src/utils/airtable.js
@@ -1,7 +1,10 @@
 import Airtable from 'airtable'
 
+const baseID = 'appVmbOc3ARWos22X' // TEST
+//const baseID = 'appyRkLfkVtG84rMU' // PRODUCTION
+
 export const base = new Airtable({
   apiKey:process.env.REACT_APP_AIRTABLE_API_KEY
-}).base('appyRkLfkVtG84rMU');
+}).base(baseID);
 
-export const MAIN_TABLE = "Data test";
+export const MAIN_TABLE = "Data";
