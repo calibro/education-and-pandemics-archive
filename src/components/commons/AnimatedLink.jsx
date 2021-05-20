fb02d9eda299bf68c04735af7bcd7f945bbce275
Daniele Pezzatini <daniele@bestiario.org>
2021-05-20 16:43:53 +0200

    Add some random lines paths

--- src/components/commons/AnimatedLink.jsx
+++ src/components/commons/AnimatedLink.jsx
@@ -2,13 +2,13 @@ import './AnimatedLink.sass';
 
 const AnimatedLink = ({text}) => {
 
+  const lines = ['#line', '#line1', '#line2']
+  const randomLine = lines[Math.floor(Math.random()*lines.length)]
+
   return (
     <a className="animated-btn" >{text}
-      <svg className="animated-btn-stroke" viewBox="0 0 430 62">
-        <use href="#line"></use>
-      </svg>
-      <svg className="animated-btn-stroke" viewBox="0 0 430 62">
-        <use href="#line" transform="scale(2, 1)"></use>
+      <svg className="animated-btn-stroke" viewBox="0 0 430 70">
+        <use href={randomLine} transform="scale(2, 1)"></use>
       </svg>
     </a>
   )
